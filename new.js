e4a5013c3aee277020eeeef05eb1cdbd9423e154
governerho <167400254+governerho@users.noreply.github.com>
2024-04-18 10:51:14 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,25 @@
+// BMI CALCULATOR
+// let weight = 50;
+// let height = 1.50;
+// let bmi = weight / (height * height);
+// console.log(`Yousuf your bmi is ${bmi}`);
+// let bmiCategory = '';
+// if (bmi < 18.5) {
+//   bmiCategory = 'Underweight';
+// } else if (bmi >= 18.5 && bmi < 24.9) {
+//   bmiCategory = 'Normal weight';
+// } else if (bmi >= 25.0 && bmi < 29.9) {
+//   bmiCategory = 'Overweight';
+// } else if (bmi >= 30.0) {
+//   bmiCategory = 'Obese';
+// }
+// console.log(`Yousuf your BMI category is ${bmiCategory}`);
+// FUNCTIONS
+function karahi(masala, beef) {
+    let recipe = masala + beef + 4;
+    console.log(recipe);
+    return recipe;
+}
+let response = karahi(2, 3);
+console.log(response);
+export {};
